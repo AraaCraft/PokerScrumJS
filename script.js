f7f307c608be6a7224492e3522d787ccfb4fc064
AraaCraft <brun.margaux66@gmail.com>
2025-04-05 01:08:27 +0200

    Add toogle on click card | Add colors on cards | Add library Darkmode.js

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
 // script.js
 
 // Array Cards Poker Scrum
-let cards = ['0', '0.5', '1', '2', '3', '5', '8', '13', '20', '40', '100', 'Coffee pls', 'Holidays'];
+let cards = ['0', '0.5', '1', '2', '3', '5', '8', '13', '20', '40', '100', '', ''];
 
 
 // Create each card with JS
 let cardContainer = document.getElementById('cardContainer');
-for (let i = 0; i <= (cards.length)-1; i++) {		//Loop to scan cards array
 
-	let newCard = document.createElement('button');	//Create new "button" card
-	newCard.innerHTML = cards[i];					// from cards array
-	cardContainer.appendChild(newCard);
+//Loop to scan cards array
+for (let i = 0; i <= (cards.length)-1; i++) {		
+
+	let newCard = document.createElement('button');	//Create new "button" card.
+	newCard.innerHTML = cards[i];					// from cards array.
+	cardContainer.appendChild(newCard);				// Add in this element.
 
 	newCard.addEventListener('click', () => scaleCard(newCard))	// Call scaleCard function when card was clicked
+
+	// To apply green color on first cards
+	if (i < 6) {
+		green(newCard)		// Call green function to color this cards.
+	}
+	// To apply yellow color on middle cards
+	else if (i < 9) {
+		yellow(newCard)		// Call yellow function to color this cards.
+	}
+	// To apply redcolor on last cards
+	else if (i < 11) {
+		red(newCard)		// Call red function to color this cards.
+	}
+	// To apply coffee image on coffee card
+	else if (i === 11) {
+		coffee(newCard)		// Call coffee function to color this card.
+	}
+	// To apply holidays image on holidays card
+	else {
+		holidays(newCard)	// Call holidays function to color this card.
+	}
 }
 
 
 // Fuction to scale cards on click
-function scaleCard(scale) {
-	scale.classList.add('scale');
-	console.log(scale);		// Debug
+function scaleCard(card) {
+	card.classList.toggle('scale');	// To alternate enter scale et reduce
+}
+
+// Function to apply green background on cards
+function green(card) {
+	card.classList.add('green');
+}
+
+// Function to apply yellow background on cards
+function yellow (card) {
+	card.classList.add('yellow');
+}
+
+// Function to apply red background on cards
+function red (card) {
+	card.classList.add('red');
+}
+
+// Function to apply coffe background on cards
+function coffee (card) {
+	card.classList.add('coffee');
+}
+
+// Function to apply holidays background on cards
+function holidays (card) {
+	card.classList.add('holidays');
 }
