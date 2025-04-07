af4786fc81324f25ff37f5963d4754a6c3fe7870
AraaCraft <brun.margaux66@gmail.com>
2025-04-07 16:06:55 +0200

    Added features to prevent background cards from being clicked || Added a sound animation when clicking on a card

--- script.js
+++ script.js
@@ -39,31 +39,59 @@ for (let i = 0; i <= (cards.length)-1; i++) {
 }
 
 
-// Fuction to scale cards on click
+
+			//ANIMATION CARDS FUNCTIONS
+
+const clickSound = new Audio('assets/sounds/arcade-ui-6-pixabay.mp3'); // Remplace le chemin par le tien
+
+// Add clic event for each button
+const buttons = document.querySelectorAll('button');		// Variable who collect all buttons
+
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		clickSound.play(); // Play sound when clic
+	});
+});
+
+// Fuction to scale cards on clic
 function scaleCard(card) {
-	card.classList.toggle('scale');	// To alternate enter scale et reduce
+	const allCards = document.querySelectorAll('button');		// Variable to select all cards
+	const alreadyScale = card.classList.contains('scale');		// Verify if already scale
+
+	// If card already scale, go to allow reduce
+	if (alreadyScale) {
+		card.classList.remove('allowClic');
+		allCards.forEach(c => c.classList.remove('blockClic'));
+	}
+	// Else, go to scale
+	else {
+		allCards.forEach(c => c.classList.add('blockClic'));
+		card.classList.remove('blockClic');
+		card.classList.add('allowClic');
+	}
+	card.classList.toggle('scale');	// To alternate enter scale and reduce
 }
+
+
+
+			//CARDS BACKGROUND FUNCTIONS
 
 // Function to apply green background on cards
 function green(card) {
 	card.classList.add('green');
 }
-
 // Function to apply yellow background on cards
 function yellow (card) {
 	card.classList.add('yellow');
 }
-
 // Function to apply red background on cards
 function red (card) {
 	card.classList.add('red');
 }
-
 // Function to apply coffe background on cards
 function coffee (card) {
 	card.classList.add('coffee');
 }
-
 // Function to apply holidays background on cards
 function holidays (card) {
 	card.classList.add('holidays');
